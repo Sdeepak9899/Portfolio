23b3d2052527e0f06b63d0f35394d5140f327157
Deepak <deepakom1593@gmail.com>
2025-09-04 14:17:31 +0530

    feat: Add Education, Experience, Skills, Work, Navbar, Footer components and constants for portfolio
    - Implemented Education component to display educational background with a timeline layout.
    - Developed Experience component to showcase work experience with a responsive design.
    - Created Skills component to highlight technical skills using logos and categories.
    - Built Work component to present projects with modal functionality for detailed views.
    - Added Navbar component for smooth scrolling navigation and responsive design.
    - Integrated Footer component with social media links and navigation.
    - Established constants.js for managing skills, experiences, education, and project data.
    - Configured Tailwind CSS for styling and responsive design.
    - Set up Vite for development environment with React support.

---
+++ src/constants.js
@@ -0,0 +1,298 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+// import webverseLogo from './assets/company_logo/webverse_logo.png';
+// import agcLogo from './assets/company_logo/agc_logo.png';
+// import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+import ashatech from './assets/company_logo/ashatech.webp';
+
+// Education Section Logo's
+// import glaLogo from './assets/education_logo/gla_logo.png';
+// import bsaLogo from './assets/education_logo/bsa_logo.png';
+// import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+// import githubdetLogo from './assets/work_logo/github_det.png';
+// import csprepLogo from './assets/work_logo/cs_prep.png';
+// import movierecLogo from './assets/work_logo/movie_rec.png';
+// import taskremLogo from './assets/work_logo/task_rem.png';
+// import npmLogo from './assets/work_logo/npm.png';
+// import webverLogo from './assets/work_logo/web_dig.png';
+// import cmLogo from './assets/work_logo/cm.png';
+// import imagesearchLogo from './assets/work_logo/image_search.png';
+// import removebgLogo from './assets/work_logo/remove_bg.png';
+import AshaLearnology from './assets/work_logo/AshaLearnology.png';
+import AshaTech from './assets/work_logo/AshaTech.png';
+import Bemins from './assets/work_logo/Bemins.png';
+import keepnotes from './assets/work_logo/keepnotes.png';
+import sowhot from './assets/work_logo/sowhot.png';
+import WaveVisas from './assets/work_logo/WaveVisas.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    // {
+    //   id: 0,
+    //   img: webverseLogo,
+    //   role: "Fullstack Developer",
+    //   company: "Webverse Digital",
+    //   date: "April 2024 - Present",
+    //   desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "JavaScript",
+    //     "React JS",
+    //     "TypeScript",
+    //     "Node JS",
+    //     "Tailwind CSS",
+    //     "MongoDb",
+    //     "Redux",
+    //     " Next Js",
+    //   ],
+    // },
+    // {
+    //   id: 1,
+    //   img: agcLogo,
+    //   role: "Fullstack Engineer",
+    //   company: "Agumentik Group of Companies",
+    //   date: "July 2023 - March 2024",
+    //   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    //   skills: [
+    //     "ReactJS",
+    //     "Redux",
+    //     "JavaScript",
+    //     "Tailwind CSS",
+    //     "HTML",
+    //     "CSS",
+    //   ],
+    // },
+    {
+      id: 2,
+      img: ashatech,
+      role: "Frontend Intern",
+      company: "Asha Tech",
+      date: "Noverber 2024 - july 2025",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Tailwind, and Material UI, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "React.js",
+        "Tailwind CSS",
+        "Next.js",
+        "Redux",
+        "Figma",
+        "Material UI",
+        "Git",
+        "GitHub",
+      ],
+    },
+  ];
+  
+  // export const education = [
+  //   {
+  //     id: 0,
+  //     img: glaLogo,
+  //     school: "GLA University, Mathura",
+  //     date: "Sept 2022 - July 2024",
+  //     grade: "7.81 CGPA",
+  //     desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
+  //     degree: "Master of Computer Applications - MCA",
+  //   },
+  //   {
+  //     id: 1,
+  //     img: bsaLogo,
+  //     school: "BSA College, Mathura",
+  //     date: "Sept 2018 - Aug 2021",
+  //     grade: "73.2%",
+  //     desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+  //     degree: "Bachelor of Science - BSC (Computer Science)",
+  //   },
+  //   {
+  //     id: 2,
+  //     img: vpsLogo,
+  //     school: "Vatsalya Public School Govardhan, Mathura",
+  //     date: "Apr 2017 - March 2018",
+  //     grade: "78%",
+  //     desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+  //     degree: "CBSE(XII) - PCM with Computer Science",
+  //   },
+  //   {
+  //     id: 3,
+  //     img: vpsLogo,
+  //     school: "Vatsalya Public School Govardhan, Mathura",
+  //     date: "Apr 2015 - March 2016",
+  //     grade: "87.5%",
+  //     desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
+  //     degree: "CBSE(X), Science with Computer Application",
+  //   },
+  // ];
+  
+  export const projects = [
+    // {
+    //   id: 0,
+    //   title: "GitHub Profile Detective",
+    //   description:
+    //     "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+    //   image: githubdetLogo,
+    //   tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+    //   github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
+    //   webapp: "https://githubprofiledetective.netlify.app/",
+    // },
+    {
+      id: 1,
+      title: "Bemins",
+      description:
+        "Bemins is a fashion e-commerce platform offering trendy apparel and accessories, featuring seasonal collections, bestsellers, and curated categories. The store delivers a modern shopping experience with sleek design, product showcases",
+      image: Bemins,
+      tags: ["React JS", "HTML", "Tailwind", "JavaScript"],
+      github: "https://github.com/Sdeepak9899",
+      webapp: "https://bemins.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "sowhot",
+      description:
+        "Sowhot.in is a sustainable fashion e-commerce brand blending Italian-Indian design roots, serving up eco-conscious activewear, denim, sleepwear, and swimwear. Ethically made in India, small-batch or made-to-order styles ship in under 10 days and come with easy returns.",
+      image: sowhot,
+      tags: ["Next JS", "API", "HTML", "CSS","Tailwind", "JavaScript","Node.js", "MongoDB", "Express"],
+      github: "https://github.com/Sdeepak9899",
+      webapp: "https://sowhot.in/",
+    },
+    {
+      id: 3,
+      title: "Asha Learnology",
+      description:
+        "Asha Learnology is a Delhi-based tech training platform offering hands-on courses in AI, ML, web & mobile development, and data science & analytics—designed to upskill learners and boost career growth.",
+      image: AshaLearnology,
+      tags: ["React JS", "Node.js","Tailwind","Framer Motion","Node.js", "MongoDB", "Express"],
+      github: "https://github.com/Sdeepak9899",
+      webapp: "https://ashalearnology-vite.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "AshaTech",
+      description:
+        "AshaTech is a Delhi NCR–based digital agency crafting sleek web and mobile solutions—plus UI/UX design, SEO, and strategic IT consulting—to help businesses level up in the online world.",
+      image: AshaTech,
+      tags: ["React JS", "Node.js","Tailwind","Framer Motion","Node.js", "MongoDB", "Express"],
+      github: "https://github.com/Sdeepak9899",
+      webapp: "https://ashatech.co.in/",
+    },
+    {
+      id: 5,
+      title: "Wave Visas",
+      description:
+        "Wave Visas is a Delhi-based immigration agency making global dreams happen. They handle PR and visa applications—for countries like Canada, UK, Australia, and more—with expert help on resumes, documents, job search, and post-landing support",
+      image: WaveVisas,
+      tags: ["React JS", "Node.js","Tailwind","Framer Motion","Node.js", "MongoDB", "Express", "JavaScript", "Framer Motion"],
+      github: "https://github.com/Sdeepak9899",
+      webapp: "https://wave-visas.vercel.app/", 
+    },
+    // {
+    //   id: 6,
+    //   title: "Coding Master",
+    //   description:
+    //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+    //   image: cmLogo,
+    //   tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+    //   github: "https://github.com/Sdeepak9899",
+    //   // webapp: "https://codingmasterweb.in/",
+    // },
+    // {
+    //   id: 7,
+    //   title: "Image Search App",
+    //   description:
+    //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    //   image: imagesearchLogo,
+    //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    //   github: "https://github.com/Sdeepak9899",
+    //   // webapp: "https://imagsearch.netlify.app/",
+    // },
+    {
+      id: 8,
+      title: "keep-notes",
+      description:
+        "A minimalist Next-based note-taking app for quick capture and organization, featuring intuitive note creation, archiving, and deletion—perfect for keeping your thoughts handy.",
+      image: keepnotes,
+      tags: ["React", "HTML", "Tailwind", "Javascript"],
+      github: "https://github.com/Sdeepak9899",
+      webapp: "https://keep-notes-flax.vercel.app/",
+    },
+  ];  
